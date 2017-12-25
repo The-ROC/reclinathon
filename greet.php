772882465d6688ac07e700f3d1335725357196fd
Chris Antimary <antimary@gmail.com>
2017-12-25 15:22:53 -0500

    Greet font size

--- greet.php
+++ greet.php
@@ -10,10 +10,14 @@
 <center>
 <div class="content" align="left">
 <h1>
+<font size=7>
 Welcome to Reclinathon!
+</font>
 </h1>
 <H3>
+<font size=6>
 <a href="https://hangouts.google.com/call/YvZ0r9JcWpbXbFeUKzjAAAEE"><u>Call the Greeter</u></a> for your official welcome.
+</font>
 </H3>
 </div>
 </center>
