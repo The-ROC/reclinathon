440627303f37aa19ca03bed69de1f6f86c0c7d4d
Chris Antimary <antimary@gmail.com>
2017-12-25 14:39:24 -0500

    Testing out hangout link to call greeter

---
+++ greet.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Welcome to Reclinathon</title>
+<link rel="stylesheet" type="text/css" href="css/index_new.css" />
+</head>
+
+<body class="noborder">
+
+<div class="main">
+<center>
+<div class="content" align="left">
+<a href="https://hangouts.google.com/call/x19M3aYKuwmw_JlOv--wAAEE">Call the Greeter</a>
+</div>
+</center>
+
+</body>
+</html>
