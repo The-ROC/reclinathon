2d5cffebe254e9839ed8087bbb17559f2513e4cf
Chris Antimary <antimary@gmail.com>
2017-12-25 15:25:00 -0500

    Greet font center

--- greet.php
+++ greet.php
@@ -7,17 +7,20 @@
 <body class="noborder">
 
 <div class="main">
-<center>
 <div class="content" align="left">
 <h1>
+<center>
 <font size=7>
 Welcome to Reclinathon!
 </font>
+</center>
 </h1>
 <H3>
+<center>
 <font size=6>
 <a href="https://hangouts.google.com/call/YvZ0r9JcWpbXbFeUKzjAAAEE"><u>Call the Greeter</u></a> for your official welcome.
 </font>
+</center>
 </H3>
 </div>
 </center>
