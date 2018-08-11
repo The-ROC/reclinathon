4c4760399336e910b66bb059aae08beaf34aa279
Chris Antimary <antimary@gmail.com>
2018-08-11 01:05:12 -0700

    Initial feed mockup

---
+++ rtt/feed_mockup.php
@@ -0,0 +1,188 @@
+<HTML>
+<HEAD>
+<title>Reclinathon Tracking Technology</title>
+<link rel="stylesheet" type="text/css" href="rtt.css" />
+</HEAD>
+
+<BODY bgcolor='white' CLASS='noborder'>
+
+<!-- Logo
+<DIV CLASS='title'>
+    <CENTER>
+        <IMG SRC='images/DefaultLogo.png'>
+    </CENTER>
+</DIV>
+-->
+
+<CENTER>
+<!-- Watch Live Button
+    <A HREF='http://www.reclinathon.com/watch.php'>Watch Live</A>
+    <BR>
+    <BR>
+-->
+    <TABLE CLASS='RttFrame'>
+        <TR>
+            <TD CLASS='RttFrame'><FORM NAME='sw'>
+                <TABLE>
+                    <TR cellspacing='0'>
+                        <TH>Reclining</TH>
+                        <TH CLASS='right'>05/12/2018 5:30 PM</TH>
+                    </TR><TR>
+                        <TD>Movie:</TD>
+                        <TD>Mad Max: Fury Road <IMG BORDER='3' WIDTH='200' SRC = 'http://upload.wikimedia.org/wikipedia/en/6/6e/Mad_Max_Fury_Road.jpg' alt = 'Mad Max: Fury Road ' ></TD>
+                    </TR><TR>
+                        <TD>Time Remaining:*</TD>
+                        <TD><input type='text' name='disp2' size='18' style="border: 0px; background-color: #C6D9F1; font-size: 125%;" readonly></INPUT></TD>
+                    </TR><TR>
+                        <TD>Captain:</TD>
+                        <TD>Dude</TD>
+                    </TR>
+                </TABLE></FORM>
+            </TD>
+<!-- Entertainment cell
+            <TD CLASS='RttFrameRight'>
+                <TABLE>
+                    <TR cellspacing='0'>
+                        <TH>Entertainment</TH>
+                    </TR><TR>
+                        <TD ID='TriviaQuestion'></TD>
+                    </TR><TR>
+                        <TD ID='TriviaAnswer'></TD>
+                    </TR>
+                </TABLE>
+            </TD>
+-->
+<!-- Spacer row
+        </TR><TR>
+            <TD CLASS='RttFrame'><BR></TD>
+            <TD CLASS='RttFrameRight'><BR></TD>
+-->
+        </TR><TR>
+<!-- Movie detail cell
+            <TD CLASS='RttFrame'>
+                <TABLE>
+                    <TR cellspacing='0'>
+                        <TH>Mad Max: Fury Road  (2015)</TH>
+                        <TH CLASS='right'>97%(90)</TH>
+                    </TR><TR ID='movie782'>
+                        <TD><IMG BORDER='3' WIDTH='200' SRC = 'http://upload.wikimedia.org/wikipedia/en/6/6e/Mad_Max_Fury_Road.jpg' alt = 'Mad Max: Fury Road ' ><BR><BR><B><U><FONT SIZE='+2'>Synopsis:</FONT></U></B><BR>A woman rebels against a tyrannical ruler in postapocalyptic Australia in search for her home-land with the help of a group of female prisoners, a psychotic worshipper, and a drifter named Max.</TD>
+                        <TD><B><U><FONT SIZE='+2'>Genre(s):</FONT></U></B><BR><B> Action</B><BR><B> Thriller</B><BR><B> Sci-Fi</B><BR><B> Adventure</B><BR><B>Apocalypse</B><BR><B>Sequel</B><BR>Reclinathon Theme<BR><B>Aussie as, mate!</B><BR><B>Who runs Bartertown?</B><BR><B>Flamethrower guitars</B><BR><BR><B><U><FONT SIZE='+2'>Runtime:</FONT></U></B><BR>120 min<BR><BR><B><U><FONT SIZE='+2'>Cast and Crew:</FONT></U></B><BR>Director: George Miller<BR><BR>Tom Hardy<BR>Charlize Theron<BR>Nicholas Hoult<BR>Hugh Keays-Byrne<BR>Josh Helman<BR>Nathan Jones<BR>Zo? Kravitz<BR>Rosie Huntington-Whiteley<BR>John Howard<BR>Iota<BR>Megan Gale<BR><BR><BR><A HREF='http://www.imdb.com/title/tt1392190/' target='_blank'>IMDB</A><BR><A HREF = 'http://www.youtube.com/results?search_query=Mad Max: Fury Road trailer, hd, short' target='_blank'>Trailer</A></TD>
+                    </TR>
+                </TABLE>
+            </TD>
+-->
+            <TD CLASS='RttFrameRight'>
+                <TABLE>
+                    <TR>
+                        <TH>History</TH>
+                        <TH CLASS='right'><A HREF = 'index.php'>Go To Current</A></TH>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=468'>Mad Max: Fury Road </A></TD>
+                        <TD align='right'>05/12/2018 5:30 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=466'>Postseason</A></TD>
+                        <TD align='right'>12/28/2017 3:49 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=465'>The Legend of Old Gregg</A></TD>
+                        <TD align='right'>12/28/2017 3:22 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=464'>Downtime</A></TD>
+                        <TD align='right'>12/28/2017 3:16 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=463'>The Lego Batman Movie</A></TD>
+                        <TD align='right'>12/28/2017 1:31 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=462'>Downtime</A></TD>
+                        <TD align='right'>12/28/2017 1:15 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=461'>Passengers </A></TD>
+                        <TD align='right'>12/28/2017 11:21 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=460'>Downtime</A></TD>
+                        <TD align='right'>12/28/2017 11:08 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=459'>Logan </A></TD>
+                        <TD align='right'>12/28/2017 8:48 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=458'>Emergency Maintenance</A></TD>
+                        <TD align='right'>12/28/2017 8:44 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=457'>Logan </A></TD>
+                        <TD align='right'>12/28/2017 8:43 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=456'>Downtime</A></TD>
+                        <TD align='right'>12/28/2017 8:23 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=455'>Office Christmas Party</A></TD>
+                        <TD align='right'>12/28/2017 6:38 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=454'>Downtime</A></TD>
+                        <TD align='right'>12/28/2017 6:37 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=453'>Kingsman: The Golden Circle</A></TD>
+                        <TD align='right'>12/28/2017 4:16 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=452'>Downtime</A></TD>
+                        <TD align='right'>12/28/2017 4:09 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=451'>Get Out </A></TD>
+                        <TD align='right'>12/28/2017 2:25 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=450'>Downtime</A></TD>
+                        <TD align='right'>12/28/2017 2:16 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=449'>Masterminds</A></TD>
+                        <TD align='right'>12/28/2017 12:42 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=448'>Downtime</A></TD>
+                        <TD align='right'>12/28/2017 12:30 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=447'>Doctor Strange </A></TD>
+                        <TD align='right'>12/27/2017 10:36 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=446'>Emergency Maintenance</A></TD>
+                        <TD align='right'>12/27/2017 10:30 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=445'>Doctor Strange </A></TD>
+                        <TD align='right'>12/27/2017 10:27 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=444'>Downtime</A></TD>
+                        <TD align='right'>12/27/2017 10:03 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=443'>Arrival</A></TD>
+                        <TD align='right'>12/27/2017 8:08 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=442'>Downtime</A></TD>
+                        <TD align='right'>12/27/2017 7:39 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=441'>The Big Sick </A></TD>
+                        <TD align='right'>12/27/2017 5:40 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=440'>Downtime</A></TD>
+                        <TD align='right'>12/27/2017 5:18 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=439'>Dunkirk</A></TD>
+                        <TD align='right'>12/27/2017 3:34 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=438'>Downtime</A></TD>
+                        <TD align='right'>12/27/2017 3:16 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=437'>Mad Max: Fury Road </A></TD>
+                        <TD align='right'>12/27/2017 1:16 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=436'>Preseason:Final Countdown</A></TD>
+                        <TD align='right'>12/26/2017 1:00 PM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=435'>Preseason:Election</A></TD>
+                        <TD align='right'>12/11/2017 12:00 AM</TD>
+                    </TR><TR>
+                        <TD><A HREF = 'index.php?ContextID=434'>Preseason:Preparing the Ballots</A></TD>
+                        <TD align='right'>12/10/2017 12:00 AM</TD>
+                    </TR>
+                </TABLE>
+            </TD>
+        </TR>
+    </TABLE>
+</CENTER>
+</BODY>
+</HTML>
