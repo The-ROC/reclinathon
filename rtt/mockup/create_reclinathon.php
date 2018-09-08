4bc6d3d244455b185e457327d1bf85c140a3f250
Chris Antimary <antimary@gmail.com>
2018-09-07 22:45:27 -0700

    Rough create reclinathon prototype with netflix search

--- rtt/mockup/create_reclinathon.php
+++ rtt/mockup/create_reclinathon.php
@@ -1,3 +1,40 @@
+<?php
+if(isset($_GET['s'])) {
+    $s = $_GET['s'];
+
+    $url = "https://www.netflix.com/api/shakti/va91c86c0/pathEvaluator?drmSystem=widevine&isWatchlistEnabled=false&isShortformEnabled=false&isVolatileBillboardsEnabled=false&falcor_server=0.1.0&withSize=true&materialize=true";
+    $data = '{"paths":[["search","byTerm","|' . $s . '","titles",48,["id","length","listId","name","referenceId","requestId","trackIds"]],["search","byTerm","|' . $s . '","titles",48,{"from":0,"to":48},"summary"],["search","byTerm","|' . $s . '","titles",48,{"from":0,"to":48},"reference",["promoVideo","summary","title","titleMaturity","userRating","userRatingRequestId"]],["search","byTerm","|' . $s . '","titles",48,{"from":0,"to":48},"reference","boxarts","_342x192","webp"]],"authURL":"1536265599850.Nw0CQSLoth8eNtVv81hYyQNOT8U="}';
+
+    $headers = [
+        'Content-Type: application/json',
+        'Cookie: memclid=c258d1f5-41f6-424b-9bd1-3b8eeacdd3c0; pas=%7B%22supplementals%22%3A%7B%22muted%22%3Atrue%7D%7D; _ga=GA1.2.357791807.1533583894; VisitorId=002~c258d1f5-41f6-424b-9bd1-3b8eeacdd3c0~1533583893452~true~1533583894505~; fbm_163114453728333=base_domain=.netflix.com; clSharedContext=a4a8cdce-5079-43de-b86f-0120d4838de8; fbsr_163114453728333=8CBkccDHE6wUbBWQycmZFZhWAQhAZ8p_ZXPku3j088o.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUUF2UTFFWE56eWlhVTdma1RYV0k2NkpJM25Sa2pCS3pDX0JhNFdOWnJGeTJPMkdjbHJiY3BxUUcxbVdDcHAxV29Sal9NTVhnRkhQeEtwRzZTR3VGOUJzWWtrSzFwd1d5QldxNXhLelNaNUZHLXVNRnFsTUtYaDZVV20zWnBlYlVUcVRUOXNNLVhseTVNbFRfVTB2T0NUaUJXb2ZxV1ZqZkRkYVloaE1lWFdYX3VlS2U0NjA0Ukw4OWdBbU9nRGNJdGh2OXpuRnZIOTVhRUZpQlZkNXZQcUZwSGJzM3J6cHotRnREbUlVXy1RNExCLVoxVlFGYkdtNE16U2lia3ctdElxaXlrblRMU1pRcG9fcjNRYVZLRGdoalo4ZG1pVE5sYmZmSXRGUHoyZ2J0Q05OOV9iSHhXeGhER09ENTNFcHZqYUVfMjRqeXUwejVreFc1aGNCWEE5MCIsImlzc3VlZF9hdCI6MTUzNjIwMjE3NSwidXNlcl9pZCI6IjQ4MDU0MzEifQ; nfvdid=BQFmAAEBEK23KnyK2MUQbFC2jYmcCRVgB7KpDdLrRazjPGbuizgt5lDksLzkF2BEmson9J%2FWFCNNgq7edgKb25jiJCG9NUemS9XsHiZxHCyK1wn98uj4a%2Bf0wec43S2R3Eq86JsOTBBk1qetAxC0l7jycUT%2BTIBD; cL=1536202332554%7C153620217844003280%7C15362021781745376%7C%7C10%7CTZONSL62FNDF3AWJRDGLY7ZKZE; SecureNetflixId=v%3D2%26mac%3DAQEAEQABABT9OUTk3tD9l4tqQjyBlGznWlYb03opzi0.%26dt%3D1536250316238; NetflixId=ct%3DBQAOAAEBEDrOCEsPB68yAjc5c4civ16B8H8E2pJPdLEe1pNPhbYImcZQpJqmrDIijykZhXNHG5eYG8VaYoGrB3eou87GvXoslvQI2OGJXJDRCvjhzq8qXIymuQojPvvcrw02nStYTnHhxs6n825kQNft8_gfmLFlSQ0X4p5uhNTufBtb_ygByod_Xp37fRANDJtIrSjFN_qrE9pBqpxkRCNaFqn4zQdO9mCVwNpoLFSyziW4T4IDuHSoU0ESW9tWKITzJEWRLLazF0QBwldCnHGY8QJPX3Vj9_UM-Nwv8n8YZLX7PXfWi-7HRgvopVWoB2x7yyPr-sfwjVCUQiDZ4mfRJsa0ojT_rIk4RrduSNFDzAOCZ6T0mgOzRIT5ASxE7-xCVRbiRpZish7T95KxOSrMzx2-8Z1PSwn54zhyDFxVL4GFpUYFL-TDX2gFQG1EGXaK7BGhU3JlLsuMUKhDvOczOpNlCwcj-9-74HT91-j0u2CadxtZsaD1UyyF3CupH_u3sSAoKymMKquVBjKt4yIuZKBIAynjvgvmr9WmLECNooRuGFavCKOnbzic2fv8WSF1O757j9MCQLb-SWDoyAVDMYDQoi7zQaR3orTRoJqxaXnU0D4CiVZJdm3gnpXFe8vRfAlM_-SDO3EJxREPrhOQS-_iWljSr9WRkyKT-CFAsaV5k4JQTlU.%26bt%3Ddbl%26ch%3DAQEAEAABABTsvmjJOn4m4ez0BNISWm6UsGeVTe59RSk.%26v%3D2%26mac%3DAQEAEAABABTig08UufJpmozR3FMJv3ncOfri_iyWu24.; profilesNewSession=0; lhpuuidh-browse-TZONSL62FNDF3AWJRDGLY7ZKZE=US%3AEN-US%3A96e55b49-1f27-46f5-b61b-09fe1b3fb1d1_ROOT; lhpuuidh-browse-TZONSL62FNDF3AWJRDGLY7ZKZE-T=1536265600916; playerPerfMetrics=%7B%22uiValue%22%3A%7B%22throughput%22%3A24760%2C%22throughputNiqr%22%3A0.28497777956803544%7D%2C%22mostRecentValue%22%3A%7B%22throughput%22%3A24760%2C%22throughputNiqr%22%3A0.28497777956803544%7D%7D',
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    echo(curl_exec($ch));
+    curl_close($ch);
+    exit();
+}
+
+if(isset($_GET['img'])) {
+    $url = $_GET['img'];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    echo(curl_exec($ch));
+    curl_close($ch);
+    exit();
+}
+?>
+
 <html>
 
 <head>
@@ -13,10 +50,82 @@
         Complete the form below to create a Reclinathon for millions of dudes to attend from all over the world!<br />
         <br />
         <form>
-            Add a movie: <input type="text" name="movielink" /><br />
-            Start time: <input type="datetime-local" name="starttime" /><br />
+            Start time<br />
+            <input type="datetime-local" name="starttime" /><br />
+            <br />
+            Movies<br />
+            <div class="dropdown">
+            <input type="text" name="movielink" style="width:360px" oninput="getSearchResults(this.value)" />
+                <div id="testDropdown" class="dropdown-content"></div>
+            </div>
+            <br />
             <br />
             <button class="button" type="submit" formaction="feed_mockup.php">Create Reclinathon</button>
         </form>
     </div>
 </body>
+
+<script>
+function getSearchResults(s) {
+    var xhReq = createXMLHttpRequest();
+    xhReq.open("GET", "create_reclinathon.php?s=" + s);
+    xhReq.onreadystatechange = function() {
+        if (xhReq.readyState != 4) { return; }
+
+        var myArr = JSON.parse(this.responseText);
+        console.log(myArr);
+
+        var testDiv = document.getElementById('testDropdown');
+        if(!testDiv.classList.contains("show"))
+            testDiv.classList.toggle("show");
+
+        testDiv.innerHTML = "";
+
+        if('byReference' in myArr.jsonGraph.search) {
+
+            var movieList = getMovieList(myArr);
+
+            var count = 0;
+            for(var i in movieList) {
+                if(!Number.isNaN(Number(i))) {
+                    var movieId = getMovieId(i, movieList);
+                    var artUrl = getBoxArtUrl(movieId, myArr);
+                    testDiv.innerHTML += "<a style='vertical-align:middle; min-height:50' href='" + getMovieUrl(movieId) + "'>" + 
+                        "<img style='float:left' src='create_reclinathon.php?img=" + encodeURIComponent(artUrl) + "' height=50/>" + 
+                        movieList[i].summary.value.name + 
+                        "</a>";
+                    count++;
+                    if(count >= 6)
+                        break;
+                }
+            }
+        }
+    };
+    xhReq.send();
+}
+
+function getMovieList(json) {
+    var ref = json.jsonGraph.search.byReference;
+    var refKey = Object.keys(ref)[0];
+    return ref[refKey];
+}
+
+function getMovieId(i, movieList) {
+    return movieList[i].summary.value.id;
+}
+
+function getBoxArtUrl(movieId, json) {
+    return json.jsonGraph.videos[movieId].boxarts["_342x192"].webp.value.url;
+}
+
+function getMovieUrl(movieId) {
+    return "https://www.netflix.com/watch/" + movieId;
+}
+
+function createXMLHttpRequest() {
+	try { return new XMLHttpRequest(); } catch(e) {}
+	try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {}
+	alert("XMLHttpRequest not supported");
+	return null;
+}
+</script>
