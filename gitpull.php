f28465a85c667686ceecc2de497c00c1b841e9e7
LA-Z-DUDE <lazdude@Chriss-MacBook-Pro-7.local>
2020-07-20 22:23:23 -0700

    Log errors in gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -28,7 +28,7 @@
 	$output = '';
 	foreach($commands AS $command){
 		// Run it
-		$tmp = shell_exec($command);
+		$tmp = shell_exec($command . ' 2>&1');
 		// Output
 		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
 		$output .= htmlentities(trim($tmp)) . "\n";
