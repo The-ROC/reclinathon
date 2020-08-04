ad95b2b0d3ac64867023aaecde02847acee33a7e
Chris Antimary <antimary@gmail.com>
2020-08-04 11:32:18 -0700

    Configure video call app to work locally or remotely

--- labs/Video-Call-App/js/config.js
+++ labs/Video-Call-App/js/config.js
@@ -5,8 +5,17 @@
 
 'use strict';
 
+let loc = window.location;
+let baseUrl = loc.protocol + '//' + loc.host + '/';
+let splitPath = loc.pathname.split('/');
+for (let i=0; i<splitPath.length; i++) {
+    // Skip empty entry @ beginning and page name @ end
+    if (i == 0 || i == splitPath.length-1) { continue; }
 
-const appRoot = 'http://localhost/reclinathon/labs/Video-Call-App/';
+    baseUrl += splitPath[i] + '/';
+}
+
+const appRoot = baseUrl;
 const wsUrl = 'ws://localhost:8080';//use wss://localhost:8080/comm for secured connection
 const wsUrl2 = 'ws://10.0.0.235:8080';
 const spinnerClass = 'fa fa-spinner faa-spin animated';
--- labs/Video-Call-App/ws/bin/server.php
+++ labs/Video-Call-App/ws/bin/server.php
@@ -13,14 +13,14 @@ use Amir\Comm;
 use Ratchet\App;
 
 //set an array of origins allowed to connect to this server
-$allowed_origins = ['localhost', '127.0.0.1', '10.0.0.194'];
+//$allowed_origins = ['localhost', '127.0.0.1', '10.0.0.194'];
 
 // Run the server application through the WebSocket protocol on port 8080
-$app = new App('localhost', 8080, '0.0.0.0');//App(hostname, port, 'whoCanConnectIP', '')
+$app = new App('localhost', 8080, '0.0.0.0'); //App(hostname, port, 'whoCanConnectIP', '')
 
 //create socket routes
 //route(uri, classInstance, arrOfAllowedOrigins)
-$app->route('/comm', new Comm, $allowed_origins);
+$app->route('/comm', new Comm, ['*']);
 
 //run websocket
 $app->run();
