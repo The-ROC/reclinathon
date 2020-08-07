6f96046d45bde447ebde19eba193e4561d5d69af
Chris Antimary <antimary@gmail.com>
2020-08-07 11:23:55 -0700

    log server name

--- labs/Video-Call-App/ws/bin/server.php
+++ labs/Video-Call-App/ws/bin/server.php
@@ -15,6 +15,7 @@ use Ratchet\App;
 //set an array of origins allowed to connect to this server
 //$allowed_origins = ['localhost', '127.0.0.1', '10.0.0.194'];
 
+echo "$_SERVER[SERVER_NAME]";
 echo "Starting server... \n";
 // Run the server application through the WebSocket protocol on port 8080
 $app = new App('localhost', 8080, '0.0.0.0'); //App(hostname, port, 'whoCanConnectIP', '')
