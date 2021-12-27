f47bb643e5cd3ac69c5c9cd284c288346b387f80
Chris Antimary <chris.antimary@streamcaptain.com>
2021-12-26 23:30:10 -0500

    Update watch live page with SyncPlay / VLC / Zoom instructions

--- watch.php
+++ watch.php
@@ -10,34 +10,37 @@ include("header.php");
 ?>
 
 <h1 style="margin-bottom:20px;">Reclinathon Live</h1>
-Participate in Reclinathon remotely using Google Hangouts on your computer or mobile device.
+Participate in Reclinathon remotely using
+<a href="https://syncplay.pl/"><strong>SyncPlay</strong></a>,
+<a href="https://www.videolan.org/vlc/"><strong>VLC Player</strong></a>,
+and
+<a href="https://zoom.us/download"><strong>Zoom</strong></a>.
 
 <h2 style="margin-bottom:5px;">Watch.</h2>
-Join our hangout to participate in Reclinathon from the comfort of your own desk recliner (you *are* sitting in a recliner by your computer, right?). Bonus points if stream the video to your TV.
+Join our SyncPlay room to watch along with us.
 <br /> <br />
-<strong>
-	<a href="https://hangouts.google.com/call/styow2upujcp3hvhninl64l5uee" target="_blank" class="live">
-		Join the Reclinathon Hangout! 
-		<br />
-		<img src="/media/webcast_preview.png" />
-	</a>
-</strong>
-
-<br />
+<strong style="color:#cccccc">SyncPlay Room:</strong> +Reclinathon:EA954A7BFD31
+<br/><br/>
+<img src="./media/syncplay_preview.png" width="400"/>
+<br/><br/>
+Messages you type into SyncPlay will appear on the movie like closed captions.<br/>
+Connecting your computer to a Reclinathon-quality TV & sound system is recommended.
 
 <h2 style="margin-bottom:5px;">Interact.</h2>
-The hangout will be muted in the arena while movies are playing, but we’ll unmute it during downtime so you can speak with the athletes. Join the arena chat to send messages while movies are playing.    
+Join our Zoom call to see arena and talk with other reclinees.
 <br /> <br />
 <strong>
-	<a href="https://hangouts.google.com/group/boM5ECwudLIJIDVW2" target="_blank" class="live">
-		Join the Arena Chat!
-		<br />
-		<img src="/media/arena_chat_preview.png" />
+	<a href="https://durhamuniversity.zoom.us/j/95974067841?pwd=Skg4Mml1ckNoWjNNRGs1VXVPMGJ1QT09
+" target="_blank" class="live">
+		Click here to join the Zoom call!
+		<br/><br/>
+		<img src="./media/webcast_preview.png"/>
 	</a>
 </strong>
-
-<br /><br />
-
+<br/><br/>
+During movies, please keep talking to a minimum and mute when you are not talking.<br/>
+During downtime, feel free to stay unmuted if you'd like.
+<br/><br/>
 Merry Reclinathon! Enjoy the webcast, and we hope to see you in the arena next year!
 <br /><br />
 
